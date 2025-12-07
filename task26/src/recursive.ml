670819cca8ae8ca62af42ed1ac56d4fed51ff07b
dllnnx <alenadenisova528@gmail.com>
2025-12-07 14:03:55 +0300

    change task26/recursive

--- task26/src/recursive.ml
+++ task26/src/recursive.ml
@@ -2,8 +2,8 @@ let rec find_longest_cycle_from d limit current_max current_d =
   if d >= limit then (current_d, current_max)
   else
     let cycle_length = Utils.find_cycle_length d in
-    if cycle_length > current_max then
-      find_longest_cycle_from (d + 1) limit cycle_length d
-    else find_longest_cycle_from (d + 1) limit current_max current_d
+    let (best_d, best_max) = find_longest_cycle_from (d + 1) limit current_max current_d in
+    if cycle_length > best_max then (d, cycle_length)
+    else (best_d, best_max)
 
 let find_longest_cycle limit = find_longest_cycle_from 2 limit 0 0 |> fst
